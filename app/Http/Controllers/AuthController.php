e97eea7b6e9069eda1f95b190508cfd1ae1eda66
Lucas Mateus Cruz Cardoso <lucas.cardoso@campus.unimontes.int>
2021-08-16 19:05:50 -0300

    Configuração JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,17 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login() {
+    public function login(Request $request) {
+        $credenciais = $request->all();
+
+        $token = auth('api')->attempt($credenciais);
+        
+        if($token) {
+            return response()->json(['token' => $token]);
+        } else {
+            return response()->json(['erro' => 'Usuário não encontrado'], 403);
+        }
+
         return 'login';
     }
 
